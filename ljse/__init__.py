1cfd69774d9fb9f51e0ee6aba8b7a6f1a3bc7b6f
Janez Bučar <janez.bucar@student.fis.unm.si>
2025-12-11 20:22:43 +0100

    added clean and multiple tickers by col

--- ljse/__init__.py
+++ ljse/__init__.py
@@ -4,4 +4,8 @@
 from .tickers import get_tickers
 from .history import get_ticker_data, get_tickers_column_data
 
-__all__ = ["get_tickers", "get_ticker_data", "get_tickers_column_data"]
+__all__ = [
+    "get_tickers",
+    "get_ticker_data",
+    "get_tickers_column_data",
+]
